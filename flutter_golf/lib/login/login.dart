d34c1fe4884e9e4307a260fbd27430bf57671152
Warren Strange <warren.strange@gmail.com>
2019-04-28 17:44:00 -0600

    backup the things

---
+++ flutter_golf/lib/login/login.dart
@@ -0,0 +1,5 @@
+export './bloc/bloc.dart';
+export './login_form.dart';
+export './login_button.dart';
+export './login_button.dart';
+export './login_screen.dart';
--- flutter_golf/lib/main.dart
+++ flutter_golf/lib/main.dart
@@ -1,31 +1,83 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import './teetimes/teetime_form.dart';
-import './register_page.dart';
-import './signin_page.dart';
-import './home_page.dart';
-
+import 'util/simple_bloc_delegate.dart';
+import 'package:bloc/bloc.dart';
+import 'svc/user_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'authentication_bloc/bloc.dart';
+import 'splash_screen.dart';
+import 'home_screen.dart';
+import 'login/login.dart';
 
 void main() {
-  runApp(MyApp());
+  BlocSupervisor().delegate = SimpleBlocDelegate();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class App extends StatefulWidget {
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final UserRepository _userRepository = UserRepository();
+  AuthenticationBloc _authenticationBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
+    _authenticationBloc.dispatch(AppStarted());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: SignInPage(),
-      routes: <String, WidgetBuilder>{
-        '/landingpage': (BuildContext context) => MyApp(),
-        //'/signup': (BuildContext context) => new SignupPage(),
-        '/homepage': (BuildContext context) => HomePage(),
-        '/teetime_form' : (BuildContext context) => TeeTimeForm(),
-
-      },
+    return BlocProvider(
+      bloc: _authenticationBloc,
+      child: MaterialApp(
+        home: BlocBuilder(
+          bloc: _authenticationBloc,
+          builder: (BuildContext context, AuthenticationState state) {
+            if (state is InitialAuthenticationState) {
+              return SplashScreen();
+            }
+            if (state is Unauthenticated) {
+              return LoginScreen(userRepository: _userRepository);
+            }
+            if (state is Authenticated) {
+              return HomeScreen(name: state.displayName);
+            }
+          },
+        ),
+      ),
     );
   }
+
+  @override
+  void dispose() {
+    _authenticationBloc.dispose();
+    super.dispose();
+  }
 }
+
+
+
+//
+//class MyApp extends StatelessWidget {
+//
+//
+////  @override
+////  Widget build(BuildContext context) {
+////    return new MaterialApp(
+////      home: SignInPage(),
+////      routes: <String, WidgetBuilder>{
+////        '/landingpage': (BuildContext context) => MyApp(),
+////        //'/signup': (BuildContext context) => new SignupPage(),
+////        '/homepage': (BuildContext context) => HomePage(),
+////        '/teetime_form' : (BuildContext context) => TeeTimeForm(),
+////
+////      },
+////    );
+////  }
+//}
